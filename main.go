9fc1ce9a1692feed7fe832f49641bf3d132b2847
Alex Scotland <1638309+AlexScotland@users.noreply.github.com>
2023-03-10 16:50:06 -0500

    Update main.go

--- main.go
+++ main.go
@@ -26,13 +26,27 @@ func init() {
 
 var day time.Weekday
 var day_time_set = false
-var trigger_time time.Time
+var trigger_time_hour int
+var trigger_time_minute int
+var timezone string
+var channel_id string
 
-func hourly_day_check() {
+func check_day_and_time() bool {
+	var today = time.Now()
+	if today.Weekday() == day && today.Hour() == trigger_time_hour && today.Minute() == trigger_time_minute {
+		return true
+	}
+	return false
+}
+
+func hourly_day_check(s *discordgo.Session) {
 	for {
+		var is_time = check_day_and_time()
 		if day_time_set {
-			time.Sleep(1 * time.Second)
-			fmt.Println("Done")
+			if is_time {
+				s.ChannelMessageSend(channel_id, "YOU READY TO GAMER?!")
+				time.Sleep(1 * time.Minute)
+			}
 		}
 	}
 }
@@ -60,17 +74,15 @@ func main() {
 
 	// Wait here until CTRL-C or other term signal is received.
 	fmt.Println("Bot is now running.  Press CTRL-C to exit.")
-	hourly_day_check()
+	hourly_day_check(dg)
 	sc := make(chan os.Signal, 1)
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
 	<-sc
-	// if time.Now().Hour() ==
-
 	// Cleanly close down the Discord session.
 	dg.Close()
 }
 
-func setup_bot(message string) (time.Weekday, int) {
+func setup_bot(message string) (time.Weekday, int, int) {
 
 	date_comparison := map[string]time.Weekday{
 		"Monday":    time.Monday,
@@ -82,14 +94,20 @@ func setup_bot(message string) (time.Weekday, int) {
 		"Sunday":    time.Sunday}
 	var extracted_data = strings.Split(message, " ")
 	day = date_comparison[extracted_data[1]]
-	trigger_time, err := strconv.Atoi(extracted_data[2])
-	if err != nil {
+	trigger_time_hour, hour_err := strconv.Atoi(extracted_data[2])
+	if hour_err != nil {
 
 		//executes if there is any error
-		fmt.Println(err)
+		fmt.Println(hour_err)
+	}
+	trigger_time_minute, min_err := strconv.Atoi(extracted_data[3])
+	if min_err != nil {
+
+		//executes if there is any error
+		fmt.Println(min_err)
 	}
 	day_time_set = true
-	return day, trigger_time
+	return day, trigger_time_hour, trigger_time_minute
 
 }
 
@@ -103,11 +121,11 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 	}
 
 	if strings.Contains(m.Content, "setup_bot") {
-		setup_bot(m.Content)
+		day, trigger_time_hour, trigger_time_minute = setup_bot(m.Content)
 		var date_string = day.String()
-		fmt.Println(trigger_time)
 		var str1 = "Bot has been configured to RollCall for " + date_string + "s"
 		s.ChannelMessageSend(m.ChannelID, str1)
+		channel_id = m.ChannelID
 	}
 
 	// If the message is "pong" reply with "Ping!"
